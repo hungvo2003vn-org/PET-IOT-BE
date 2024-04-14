9f30774bf2abfaa9a2739d01b18f526683a704cf
hungvo2003vn <votanhung2003@gmail.com>
2024-04-14 16:38:59 +0700

    DADN-42 Update some methods in stations schema

--- src/api/v1/station/controllers/getStations.js
+++ src/api/v1/station/controllers/getStations.js
@@ -3,11 +3,11 @@ import StationService from "../services/index.js"
 const getStations = async (req, res, next) => {
     try {
         const {
-            station_id
-        } = req.body
+            id
+        } = req.query
 
         const data = await new StationService().getStations({
-            station_id,
+            station_id: id,
             user_id: req.userService.userInfo._id.toString()
         })
         res.status(200).json({data})
